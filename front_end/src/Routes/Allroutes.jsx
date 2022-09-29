d26cfccb558aee36bea043b3a97b686a6d1fd452
vivek13420 <vivekrana13420@gmail.com>
2022-09-29 13:02:46 +0530

    added navbar

--- front_end/src/Routes/Allroutes.jsx
+++ front_end/src/Routes/Allroutes.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import Demo from '../Pages/Demo/Demo'
+import Home from '../Pages/Home/Home'
+import Integrations from '../Pages/Integrations/Integrations'
+import Login from '../Pages/Login'
+import Pricing from '../Pages/Pricing/Pricing'
 
 const Allroutes = () => {
   return (
       <div>
         
           <Routes>
-              <Route path="/home" element={"home"} />
-              <Route path="/pricing" element={"pricing"} />
-              <Route path="/demo" element={"demo"} />
-              <Route path="login" element={"login"} />
+              <Route path="/" element={<Home/>} />
+              <Route path="/pricing" element={<Pricing/>} />
+              <Route path="/demo" element={<Demo/>} />
+              <Route path="/integrations" element={<Integrations/>} />
+              <Route path="/login" element={"login"} />
               <Route path="/signup" element={"signup"} />
               <Route path="/dashboard" element={"dashboard"}/>
           </Routes> 
